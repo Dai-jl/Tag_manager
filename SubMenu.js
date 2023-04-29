8ed69aecd17f3e1c182c38da214576bbc84895e9
DJL <dai_jl@outlook.com>
2023-04-29 15:25:56 +0800

    add menu item

---
+++ SubMenu.js
@@ -0,0 +1,10 @@
+export class SubMenu{
+    constructor(title,icon,section){
+        this.title = title ? title : '';
+        this.icon = icon ? icon : '';
+        this.section = section ? section : '';//TODO
+    }
+    onHover(){
+        
+    }
+}
